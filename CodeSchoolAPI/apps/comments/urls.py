62f3440af6e120f0fedb8aa2ead6e04292e959e6
anonymous203030 <aram.simonyan.03@gmail.com>
2020-09-21 23:41:00 +0400

    Custom Comment List API Endpoint

--- CodeSchoolAPI/apps/comments/urls.py
+++ CodeSchoolAPI/apps/comments/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import CommentCreateViewSet, CommentListViewSet, CommentDetailViewSet
+from .views import CommentCreateViewSet, CommentListViewSet, CommentDetailViewSet, CustomCommentListViewSet
 
 urlpatterns = [
     path('create/', CommentCreateViewSet.as_view()),
     path('list/', CommentListViewSet.as_view()),
     path('detail/<int:pk>/', CommentDetailViewSet.as_view()),
+    path('common/', CustomCommentListViewSet.as_view()),
 
 ]
--- CodeSchoolAPI/apps/comments/views.py
+++ CodeSchoolAPI/apps/comments/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
+from .filters import IsOwnerFilter
 from .models import Comment
 from .permissions import IsOwner
 from .serializers import CommentSerializer
@@ -22,3 +23,9 @@ class CommentDetailViewSet(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = (IsOwner, IsAdminUser, )
+
+class CustomCommentListViewSet(generics.ListAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticated, )
+    filter_backends = (IsOwnerFilter, )
