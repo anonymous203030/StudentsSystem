2d08e95c7f65f3110e90f44245c9080ac3bd937a
anonymous203030 <aram.simonyan.03@gmail.com>
2020-09-21 23:50:07 +0400

    Basic Mistake Fixes

--- CodeSchoolAPI/apps/comments/serializers.py
+++ CodeSchoolAPI/apps/comments/serializers.py
@@ -4,9 +4,11 @@ from .models import Comment, CommentRelationship
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    model = Comment
-    fields = ('lecture', 'comment', 'owner', 'is_liked', 'created_at', )
+    class Meta:
+        model = Comment
+        fields = ('lecture', 'comment', 'owner', 'is_liked', 'created_at', )
 
 class CommentRelationshipSerializer(serializers.ModelSerializer):
-    model = CommentRelationship
-    fields = ('user', 'comment', 'like', 'liked_at', )
+    class Meta:
+        model = CommentRelationship
+        fields = ('user', 'comment', 'like', 'liked_at', )
