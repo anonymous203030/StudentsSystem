806c878bca47b3413d19e8ff4066474f1de9e521
anonymous203030 <aram.simonyan.03@gmail.com>
2020-11-20 12:04:07 +0400

    Error Fixes

--- CodeSchoolAPI/apps/users/urls.py
+++ CodeSchoolAPI/apps/users/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
+from .views import *
 
-from .views import UserViewSet, UserProfileViewSet
-
-router = routers.SimpleRouter()
-router.register(r'register', UserViewSet)
-router.register(r'profiles', UserProfileViewSet)
+p = 'profile/'
 urlpatterns = [
-    path('', include(router.urls)),
+        #USER
+    path(f'register/',RegisterViewSet.as_view(), name='user_register'),
+    path(f'login/', LoginAPiViewSet.as_view(), name='user_login'),
+    path(f'list/', UsersListViewSet.as_view(), name='user_list'),
+        #PROFILE
+    path(f'{p}create/', UserProfileCreateViewSet.as_view(), name='create_profile'),
+    path(f'{p}list/', UserProfileListViewSet.as_view(), name='list_profile'),
+    path(f'{p}detail/<int:pk>/', UserProfileDetailViewSet.as_view(), name='change_profile'),
+
 ]
--- CodeSchoolAPI/apps/users/views.py
+++ CodeSchoolAPI/apps/users/views.py
@@ -1,29 +1,75 @@
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets, status
+from django.core.exceptions import ValidationError
+from rest_framework import viewsets, status, generics, filters
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
-from .models import UserProfile
-from .serializers import UserSerializer, UserProfileSerializer
+from .models import *
+from .permissions import IsOwner
+from .serializers import *
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = get_user_model().objects.all()
+class RegisterViewSet(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        user_data = serializer.data
+
+        return Response(user_data, status=status.HTTP_201_CREATED)
+
+
+class LoginAPiViewSet(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def perform_create(self, serializer):
+        queryset = LoginSerializer.objects.filter(user=self.request.user)
+        if queryset.exists():
+            raise ValidationError('You have already signed up')
+        serializer.save(user=self.request.user)
+
+
+class UsersListViewSet(generics.ListAPIView):
+    queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = PageNumberPagination
+    filter_backends = [filters.SearchFilter,
+                       filters.OrderingFilter]
+    search_fields = ['username', 'email']
+    ordering_fields = ['created_at', 'updated_at']
+    filterset_fields = ['created_at', 'updated_at']
+    # USER PROFILE ViewSets
 
-    def get_queryset(self):
-        return self.request.user.accounts.all()
 
-class UserProfileViewSet(viewsets.ModelViewSet):
+class UserProfileListViewSet(generics.ListAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
-    permission_classes = (IsAuthenticated, )
+    permission_classes = [IsAuthenticated]
+    pagination_class = PageNumberPagination
+    filter_backends = [filters.SearchFilter,
+                       filters.OrderingFilter]
+    search_filters = ['first_name', 'last_name', 'owner']
+    ordering_fields = ['owner__created_at', 'owner__updated_at']
+    filterset_fields = ['birthday', 'gender']
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED,
-                        headers=headers)
+class UserProfileCreateViewSet(generics.CreateAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsOwner]
+
+class UserProfileDetailViewSet(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsOwner]
 
